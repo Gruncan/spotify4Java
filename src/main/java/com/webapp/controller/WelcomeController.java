ef8e37f55d51d4affab26a60da0ee038873ec776
Duncan Jones <2561101J@student.gla.ac.uk>
2022-05-24 14:58:18 +0100

    Authentication works, returns user profile

--- src/main/java/com/webapp/controller/WelcomeController.java
+++ src/main/java/com/webapp/controller/WelcomeController.java
@@ -1,16 +1,28 @@
 package com.webapp.controller;
 
+import com.github.openjson.JSONObject;
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.message.BasicNameValuePair;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.Base64;
+import java.util.List;
+import java.util.Scanner;
 
 @Controller
 public class WelcomeController {
@@ -42,7 +54,7 @@ public class WelcomeController {
     }
 
     @GetMapping("/redirect/")
-    public void redirect(@RequestParam String code, @RequestParam String state) {
+    public void redirect(@RequestParam String code, @RequestParam String state) throws URISyntaxException {
         System.out.println("In get redirect");
         System.out.println("code: " + code);
 
@@ -52,28 +64,76 @@ public class WelcomeController {
     }
 
 
-    private void fetchAccessToken(String code) {
-        String url_token = "https://accounts.spotify.com/api/token?"
-                + "grant_type=authorization_code"
-                + "&code=" + code
-                + "&redirect_uri=" + REDIRECT_URL_2
-                + "&client_id=" + CLIENT_ID
-                + "&client_secret=" + CLIENT_SECRET;
+    private void fetchAccessToken(String code) throws URISyntaxException {
+        String url_token = "https://accounts.spotify.com/api/token";
 
-        System.out.println(url_token);
+
+        List<NameValuePair> parameters = new ArrayList<>();
+        parameters.add(new BasicNameValuePair("grant_type", "authorization_code"));
+        parameters.add(new BasicNameValuePair("code", code));
+        parameters.add(new BasicNameValuePair("redirect_uri", REDIRECT_URL));
+
+        URIBuilder uriBuilder = new URIBuilder(url_token);
+        uriBuilder.addParameters(parameters);
+
+
         HttpClient httpClient = HttpClientBuilder.create().build();
         HttpResponse response;
         try {
-            HttpPost httppost = new HttpPost(url_token);
+            HttpPost httppost = new HttpPost(uriBuilder.build());
             httppost.setHeader("Content-type", "application/x-www-form-urlencoded");
-            httppost.setHeader("Authorization", Base64.getEncoder().encodeToString(String.format("Basic %s:%s", CLIENT_ID, CLIENT_SECRET).getBytes(StandardCharsets.UTF_8)));
+            httppost.setHeader("Authorization", "Basic " + new String(Base64.getEncoder().encode((CLIENT_ID + ":" + CLIENT_SECRET).getBytes(StandardCharsets.UTF_8))));
+//            httppost.setEntity(new UrlEncodedFormEntity(parameters, "UTF-8"));
+
             response = httpClient.execute(httppost);
+            HttpEntity entity = response.getEntity();
+            if (response.getStatusLine().getStatusCode() == 200) { // ok request
+                if (entity != null) {
+                    try (InputStream instream = entity.getContent()) {
+                        Scanner scanner = new Scanner(instream).useDelimiter("\\A");
+                        String result = scanner.hasNext() ? scanner.next() : "";
+                        System.out.println(result);
+                        JSONObject jsonObject = new JSONObject(result);
+                        String token = jsonObject.get("access_token").toString();
+
+                        getProfile(token);
+
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+
+
             System.out.println(response);
-
-
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private void getProfile(String token) throws IOException {
+        HttpGet get = new HttpGet("https://api.spotify.com/v1/me/");
+        get.setHeader("Content-Type", "application/json");
+        get.setHeader("Authorization", "Bearer " + token);
+
+        HttpClient httpClient = HttpClientBuilder.create().build();
+        HttpResponse response = httpClient.execute(get);
+        HttpEntity entity = response.getEntity();
+        if (entity != null) {
+            try (InputStream instream = entity.getContent()) {
+                Scanner scanner = new Scanner(instream).useDelimiter("\\A");
+                String result = scanner.hasNext() ? scanner.next() : "";
+                System.out.println(result);
+                JSONObject jsonObject = new JSONObject(result);
+                System.out.println(jsonObject.toString());
+
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+
     }
 
 
