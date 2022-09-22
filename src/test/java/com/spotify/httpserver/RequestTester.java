2bcb47dadef14d7fe41a088c662c504a5deecf37
Duncan Jones <dj223@outlook.com>
2022-09-22 22:47:50 +0100

    Added alternative to using spring

---
+++ src/test/java/com/spotify/httpserver/RequestTester.java
@@ -0,0 +1,75 @@
+package com.spotify.httpserver;
+
+import com.spotify.SpotifyClient;
+import com.spotify.SpotifyClientBuilder;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+
+public class RequestTester {
+
+    private static final String CLIENT_ID = "49d248093d8c41dd8b65aba37c5ef665";
+    private static final String CLIENT_SECRET = "381ccdf476ac4584b0d936663d5c2232";
+    private static final String REDIRECT_URL = "http://127.0.0.1:8888/redirect/";
+
+    private static SpotifyClientBuilder spotifyClientBuilder;
+    private static HttpServer server;
+
+
+    public static void main(String[] args) throws Exception {
+        Desktop desktop = Desktop.getDesktop();
+        server = HttpServer.create(new InetSocketAddress(8888), 0);
+        server.createContext("/redirect/", new RedirectHandler());
+
+
+        spotifyClientBuilder = new SpotifyClientBuilder(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL);
+        System.out.println(spotifyClientBuilder.buildAuthUrl());
+        desktop.browse(new URI(spotifyClientBuilder.buildAuthUrl()));
+        server.start();
+
+
+    }
+
+
+    private static class RedirectHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange e) throws IOException {
+            Map<String, String> requestParams = queryToMap(e.getRequestURI().getQuery());
+            String code = requestParams.get("code");
+            if (code == null) throw new IOException("IDEK");
+
+            SpotifyClient spotifyClient = spotifyClientBuilder.build(code);
+            server.stop(0);
+
+
+            // execute request
+
+        }
+
+
+        public Map<String, String> queryToMap(String query) {
+            if (query == null) {
+                return null;
+            }
+            Map<String, String> result = new HashMap<>();
+            for (String param : query.split("&")) {
+                String[] entry = param.split("=");
+                if (entry.length > 1) {
+                    result.put(entry[0], entry[1]);
+                } else {
+                    result.put(entry[0], "");
+                }
+            }
+            return result;
+        }
+
+    }
+
+}
