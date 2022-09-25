3d0a012d8969a0cc60b81e24a4e3d667fd9c6766
Duncan Jones <dj223@outlook.com>
2022-09-25 21:18:21 +0100

    Updated in requests (forgot)

--- src/main/java/com/spotify/requests/AbstractRequest.java
+++ src/main/java/com/spotify/requests/AbstractRequest.java
@@ -1,21 +1,15 @@
 package com.spotify.requests;
 
+import com.http.HttpMethod;
+import com.http.HttpRequest;
+import com.http.HttpResponse;
 import com.spotify.json.JSONObject;
 import com.spotify.requests.util.ParameterPair;
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.StatusLine;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.HttpClientBuilder;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.io.Serializable;
 import java.lang.reflect.ParameterizedType;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Scanner;
 
 
 /**
@@ -25,7 +19,6 @@ public abstract class AbstractRequest<T extends Serializable> extends SpotifySer
 
 
     private final static String BASE_URL = "https://api.spotify.com/v1/";
-    private final static HttpClient httpClient = HttpClientBuilder.create().build();
     private final Map<String, RequestQuery<?>> queries;
     private final Map<String, Class<?>> restrictedQueryTypes;
 
@@ -52,68 +45,46 @@ public abstract class AbstractRequest<T extends Serializable> extends SpotifySer
      */
     protected final JSONObject requestGet(String token, String url) {
         // Initialisation of http get request
-//        System.out.println(BASE_URL + url);
 
-        StringBuilder getUrl = new StringBuilder(BASE_URL + url);
 
-        if (!this.queries.isEmpty()) {
-            getUrl.append("?");
-            for (RequestQuery<?> query : this.queries.values()) {
-                getUrl.append(query.getPair()).append("&");
-            }
-            getUrl.deleteCharAt(getUrl.length() - 1);
+        HttpRequest request = new HttpRequest(BASE_URL + url, HttpMethod.GET);
+        request.addRequestHeader("Authorization", "Bearer " + token);
+        request.addRequestHeader("Content-Type", "application/json");
+
+        for (RequestQuery<?> rq : this.queries.values()) {
+            request.addRequestQuery(rq.getKey(), rq.getPair());
         }
-        System.out.println(getUrl);
-        HttpGet httpGet = new HttpGet(getUrl.toString());
-        // setting headers using the token
-        httpGet.setHeader("Authorization", "Bearer " + token);
-        httpGet.setHeader("Content-Type", "application/json");
 
 
         try {
-            HttpResponse response = httpClient.execute(httpGet);
-            System.out.println(response);
-            StatusLine statusLine = response.getStatusLine();
-            HttpEntity entity = response.getEntity();
-            String s = this.processBody(entity);
-            if (statusLine.getStatusCode() == 200) {
-                // if good request then process body and return json
+            HttpResponse response = request.execute();
+            int code = response.getCode();
+            String s = response.getContent();
+            if (code == 200) {
                 return new JSONObject(s);
-                // handle other requests, taken from docs
-            } else if (statusLine.getStatusCode() == 401) {
+            } else if (code == 401) {
                 System.out.println("Bad or expired token. This can happen if the user revoked a token or the access token has expired. You should re-authenticate the user.");
-            } else if (statusLine.getStatusCode() == 403) {
+            } else if (code == 403) {
                 System.out.println("Bad OAuth request (wrong consumer key, bad nonce, expired timestamp...). Unfortunately, re-authenticating the user won't help here.");
-            } else if (statusLine.getStatusCode() == 429) {
+            } else if (code == 429) {
                 System.out.println("The app has exceeded its rate limits.");
-            } else if (statusLine.getStatusCode() == 400) {
+            } else if (code == 400) {
                 System.out.println("Bad request.");
-            } else if (statusLine.getStatusCode() == 404) {
+            } else if (code == 404) {
                 System.out.println("Unknown request");
             } else {
-                System.out.printf("Unknown fail cause, status code: %s.%n", statusLine.getStatusCode());
+                System.out.printf("Unknown fail cause, status code: %s.%n", code);
             }
             System.out.println(s);
             return null;
 
 
-        } catch (IOException e) {
+        } catch (NullPointerException e) {
             e.printStackTrace();
             return null;
         }
     }
 
-    private String processBody(HttpEntity entity) throws IOException {
-        // reads in body of the request converting it to string
-        if (entity != null) {
-            try (InputStream inputStream = entity.getContent()) {
-                Scanner scanner = new Scanner(inputStream).useDelimiter("\\A");
-                String result = scanner.hasNext() ? scanner.next() : null;
-                return result;
-            }
-        } else
-            return null;
-    }
 
     /**
      * Adds a query key value to given request proved it is allowed.
