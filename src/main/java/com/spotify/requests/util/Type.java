80e898bb4dc09ba9ff4f3248edf2523bedeb6d86
Duncan Jones <dj223@outlook.com>
2022-12-16 20:54:48 +0000

    Fixed issue with search

--- src/main/java/com/spotify/requests/util/Type.java
+++ src/main/java/com/spotify/requests/util/Type.java
@@ -4,8 +4,8 @@ import java.util.Locale;
 
 public enum Type {
 
-    ARTISTS,
-    TRACKS;
+    ARTIST,
+    TRACK;
 
 
     @Override
--- src/test/java/com/spotify/Main.java
+++ src/test/java/com/spotify/Main.java
@@ -1,19 +1,20 @@
 package com.spotify;
 
 import com.spotify.json.JSONObject;
-import com.spotify.requests.AbstractRequest;
-import com.spotify.requests.tracks.SeveralTrackAudioFeaturesGet;
+import com.spotify.requests.search.SearchGet;
+import com.spotify.requests.util.Type;
 
 public class Main {
 
 
     public static void main(String[] args) throws IllegalAccessException {
-        SpotifyClient sc = new SpotifyClientBuilderTester().getBuiltClient();
+        SpotifyClient sc = new SpotifyClientBuilderTester().printAccessToken().getBuiltClient();
 
+        SearchGet searchGet = new SearchGet("Don't Stop artist:Fleetwood Mac", Type.TRACK);
+//        AbstractRequest al = new SeveralTrackAudioFeaturesGet("05lBXOMA1uHpVPEQZyjoh3", "1ljziaoMnRH95aPeOSGAtr1");
 
-        AbstractRequest al = new SeveralTrackAudioFeaturesGet("05lBXOMA1uHpVPEQZyjoh3", "1ljziaoMnRH95aPeOSGAtr1");
-
-        JSONObject jsonObject = sc.executeRequest(al).ok();
+        System.out.println(searchGet.buildRequestUrl());
+        JSONObject jsonObject = sc.executeRequest(searchGet).ok();
         System.out.println(jsonObject.toString(4));
 
 
