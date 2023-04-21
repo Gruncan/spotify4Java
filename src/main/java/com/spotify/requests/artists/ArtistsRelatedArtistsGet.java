52d8adb7a1e6ab7d95630a9381e1815ad1b4521d
Duncan Jones <dj223@outlook.com>
2023-04-21 21:03:19 +0100

    Added serialization to ArtistsRelatedArtistsGet

--- src/main/java/com/spotify/requests/artists/ArtistsRelatedArtistsGet.java
+++ src/main/java/com/spotify/requests/artists/ArtistsRelatedArtistsGet.java
@@ -1,10 +1,13 @@
 package com.spotify.requests.artists;
 
+import com.spotify.objects.SpotifySerialize;
+import com.spotify.objects.artists.Artist;
 import com.spotify.requests.AbstractRequest;
 import com.spotify.requests.SpotifyRequest;
 import com.spotify.requests.SpotifySubRequest;
 
 @SpotifyRequest(value = "artists", end = "related-artist")
+@SpotifySerialize(Artist.class)
 public class ArtistsRelatedArtistsGet extends AbstractRequest {
 
 
