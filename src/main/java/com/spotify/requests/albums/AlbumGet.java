380ce6c2a8cbeae8dfc7a2349b36f0d018fb308a
Duncan Jones <dj223@outlook.com>
2023-04-23 21:28:01 +0100

    Added documentation to requests.albums

--- src/main/java/com/spotify/requests/albums/AlbumGet.java
+++ src/main/java/com/spotify/requests/albums/AlbumGet.java
@@ -10,20 +10,37 @@ import com.spotify.requests.SpotifySubRequest;
 import lombok.Setter;
 
 /**
+ * Get Spotify catalog information for a single album.
+ * <a href="https://developer.spotify.com/documentation/web-api/reference/get-an-album">Spotify Docs</a>
+ * <p>Serializes into {@link Album}</p>
  *
+ * @see Album
+ * @see AbstractRequest
+ * @see SpotifyRequest
+ * @see SpotifySerialize
  */
+@Setter
 @SpotifyRequest("albums")
 @SpotifySerialize(Album.class)
-@Setter
 public class AlbumGet extends AbstractRequest {
 
+    /**
+     * The Spotify ID of the album.
+     */
     @SpotifySubRequest
     private final String id;
 
+    /**
+     * An ISO 3166-1 alpha-2 country code. If a country code is specified, only content that is available in that market will be returned.
+     */
     @SpotifyRequestField
     private Country market;
 
 
+    /**
+     * Initializes the {@link AlbumGet} request
+     * @param id The Spotify ID of the album.
+     */
     public AlbumGet(String id) {
         this.id = id;
     }
--- src/main/java/com/spotify/requests/albums/SeveralAlbumsGet.java
+++ src/main/java/com/spotify/requests/albums/SeveralAlbumsGet.java
@@ -8,18 +8,38 @@ import com.spotify.requests.SpotifyRequest;
 import com.spotify.requests.SpotifyRequestField;
 import lombok.Setter;
 
+/**
+ * Get Spotify catalog information for multiple albums identified by their Spotify IDs.
+ * <a href="https://developer.spotify.com/documentation/web-api/reference/get-multiple-albums">Spotify Docs</a>
+ * <p>Serializes into {@link Album}</p>
+ *
+ * @see Album
+ * @see AbstractRequest
+ * @see SpotifyRequest
+ * @see SpotifySerialize
+ */
 @Setter
 @SpotifyRequest("albums")
 @SpotifySerialize(value = Album.class, isArray = true)
 public class SeveralAlbumsGet extends AbstractRequest {
 
+    /**
+     * A comma-separated list of the Spotify IDs for the albums. Maximum: 20 IDs.
+     */
     @SpotifyRequestField
     private final String[] ids;
 
+    /**
+     * An ISO 3166-1 alpha-2 country code. If a country code is specified, only content that is available in that market will be returned.
+     */
     @SpotifyRequestField
     private Country market;
 
 
+    /**
+     * Initializes the {@link SeveralAlbumsGet} request
+     * @param ids A list of the Spotify IDs for the albums. Maximum: 20 IDs.
+     */
     public SeveralAlbumsGet(String... ids) {
         this.ids = ids;
         this.market = null;
