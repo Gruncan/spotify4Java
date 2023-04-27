8ca79e17a09f8a975fbd9af1bca97dce109e80cb
Duncan Jones <dj223@outlook.com>
2023-04-27 11:27:04 +0100

    Added docs to requests.shows

--- src/main/java/com/spotify/requests/shows/SeveralShowsGet.java
+++ src/main/java/com/spotify/requests/shows/SeveralShowsGet.java
@@ -1,5 +1,8 @@
 package com.spotify.requests.shows;
 
+import com.spotify.SpotifyResponse;
+import com.spotify.objects.SpotifySerialize;
+import com.spotify.objects.shows.Show;
 import com.spotify.objects.wrappers.Country;
 import com.spotify.requests.AbstractRequest;
 import com.spotify.requests.SpotifyRequest;
@@ -7,17 +10,39 @@ import com.spotify.requests.SpotifyRequestField;
 import com.spotify.requests.SpotifySubRequest;
 import lombok.Setter;
 
+/**
+ * Get Spotify catalog information for several shows based on their Spotify IDs.
+ * <a href="https://developer.spotify.com/documentation/web-api/reference/get-multiple-shows">Spotify Docs</a>
+ * <p>Serializes into {@link Show}</p>
+ *
+ * @see Show
+ * @see AbstractRequest
+ * @see SpotifyRequest
+ * @see SpotifySerialize
+ * @see SpotifyResponse
+ */
 @Setter
 @SpotifyRequest("shows")
+@SpotifySerialize(value = Show.class, isArray = true)
 public class SeveralShowsGet extends AbstractRequest {
 
 
+    /**
+     * A list of the Spotify IDs for the shows. Maximum: 50 IDs.
+     */
     @SpotifySubRequest
     private final String[] ids;
 
+    /**
+     * An ISO 3166-1 alpha-2 country code. If a country code is specified, only content that is available in that market will be returned.
+     */
     @SpotifyRequestField
     private Country market;
 
+    /**
+     * Initializes the {@link SeveralShowsGet} request
+     * @param ids A list of the Spotify IDs for the shows. Maximum: 50 IDs.
+     */
     public SeveralShowsGet(String... ids) {
         this.ids = ids;
     }
--- src/main/java/com/spotify/requests/shows/ShowEpisodesGet.java
+++ src/main/java/com/spotify/requests/shows/ShowEpisodesGet.java
@@ -1,5 +1,7 @@
 package com.spotify.requests.shows;
 
+import com.spotify.SpotifyResponse;
+import com.spotify.objects.SpotifySerialize;
 import com.spotify.objects.wrappers.Country;
 import com.spotify.requests.AbstractRequest;
 import com.spotify.requests.SpotifyRequest;
@@ -7,24 +9,51 @@ import com.spotify.requests.SpotifyRequestField;
 import com.spotify.requests.SpotifySubRequest;
 import lombok.Setter;
 
+/**
+ * Get Spotify catalog information about an show’s episodes. Optional parameters can be used to limit the number of episodes returned.
+ * <a href="https://developer.spotify.com/documentation/web-api/reference/get-a-shows-episodes">Spotify Docs</a>
+ * <p>Serializes into {@link ?}</p>
+ *
+ * @see ?
+ * @see AbstractRequest
+ * @see SpotifyRequest
+ * @see SpotifySerialize
+ * @see SpotifyResponse
+ */
 @Setter
 @SpotifyRequest(value = "shows", end = "episodes")
 public class ShowEpisodesGet extends AbstractRequest {
 
 
+    /**
+     * The Spotify ID for the show.
+     */
     @SpotifySubRequest
     private final String id;
 
+    /**
+     * An ISO 3166-1 alpha-2 country code. If a country code is specified, only content that is available in that market will be returned.
+     */
     @SpotifyRequestField
     private Country market;
 
+    /**
+     * The maximum number of items to return. Default: 20. Minimum: 1. Maximum: 50.
+     */
     @SpotifyRequestField
     private int limit;
 
+    /**
+     * The index of the first item to return. Default: 0 (the first item). Use with limit to get the next set of items.
+     */
     @SpotifyRequestField
     private int offset;
 
 
+    /**
+     * Initializes the {@link ShowEpisodesGet} request
+     * @param id The Spotify ID for the show.
+     */
     public ShowEpisodesGet(String id) {
         this.id = id;
     }
