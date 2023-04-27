af8a9d0ee53fa499b9b55568c64a6026d7d58b49
Duncan Jones <dj223@outlook.com>
2023-04-27 20:58:07 +0100

    Added docs to requests.tracks

--- src/main/java/com/spotify/requests/tracks/SeveralTrackAudioFeaturesGet.java
+++ src/main/java/com/spotify/requests/tracks/SeveralTrackAudioFeaturesGet.java
@@ -1,5 +1,6 @@
 package com.spotify.requests.tracks;
 
+import com.spotify.SpotifyResponse;
 import com.spotify.objects.SpotifySerialize;
 import com.spotify.objects.tracks.TrackAudioFeatures;
 import com.spotify.requests.AbstractRequest;
@@ -8,18 +9,30 @@ import com.spotify.requests.SpotifyRequestField;
 
 /**
  * Get audio features for multiple tracks based on their Spotify IDs.
- * <a href="https://developer.spotify.com/documentation/web-api/reference/#/operations/get-several-audio-features">Spotify Docs</a>
+ * <a href="https://developer.spotify.com/documentation/web-api/reference/get-several-audio-features">Spotify Docs</a>
+ * <p>Serializes into {@link TrackAudioFeatures} array</p>
+ *
+ * @see TrackAudioFeatures
+ * @see AbstractRequest
+ * @see SpotifyRequest
+ * @see SpotifySerialize
+ * @see SpotifyResponse
  */
-
 @SpotifyRequest("audio-features")
 @SpotifySerialize(value = TrackAudioFeatures.class, isArray = true)
 public class SeveralTrackAudioFeaturesGet extends AbstractRequest {
 
-
+    /**
+     * A list of the Spotify IDs for the tracks. Maximum: 100 IDs
+     */
     @SpotifyRequestField
     private final String[] ids;
 
 
+    /**
+     * Initializes the {@link SeveralTrackAudioFeaturesGet} request
+     * @param ids A list of the Spotify IDs for the tracks. Maximum: 100 IDs
+     */
     public SeveralTrackAudioFeaturesGet(String... ids) {
         this.ids = ids;
     }
--- src/main/java/com/spotify/requests/tracks/TrackAudioFeaturesGet.java
+++ src/main/java/com/spotify/requests/tracks/TrackAudioFeaturesGet.java
@@ -1,5 +1,6 @@
 package com.spotify.requests.tracks;
 
+import com.spotify.SpotifyResponse;
 import com.spotify.objects.SpotifySerialize;
 import com.spotify.objects.tracks.TrackAudioFeatures;
 import com.spotify.requests.AbstractRequest;
@@ -9,18 +10,28 @@ import com.spotify.requests.SpotifySubRequest;
 
 /**
  * Get audio feature information for a single track identified by its unique Spotify ID.
- * <a href="https://developer.spotify.com/documentation/web-api/reference/#/operations/get-audio-features">Spotify Docs</a>
+ * <a href="https://developer.spotify.com/documentation/web-api/reference/get-audio-features">Spotify Docs</a>
+ * <p>Serializes into {@link TrackAudioFeatures}</p>
+ *
+ * @see TrackAudioFeatures
+ * @see AbstractRequest
+ * @see SpotifyRequest
+ * @see SpotifySerialize
+ * @see SpotifyResponse
  */
 @SpotifyRequest("audio-features")
 @SpotifySerialize(TrackAudioFeatures.class)
 public class TrackAudioFeaturesGet extends AbstractRequest {
 
-
+    /**
+     * The Spotify ID for the track.
+     */
     @SpotifySubRequest
     private final String id;
 
     /**
-     * @param id The id of the song to get
+     * Initializes the {@link TrackAudioFeaturesGet} request
+     * @param id The Spotify ID for the track.
      */
     public TrackAudioFeaturesGet(String id) {
         this.id = id;
