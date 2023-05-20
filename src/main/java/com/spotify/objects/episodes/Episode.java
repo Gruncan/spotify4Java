7596bc2d726748072579f22154748abd55869dc3
Duncan Jones <dj223@outlook.com>
2023-05-20 21:01:24 +0100

    Implemented Episode object

--- src/main/java/com/spotify/objects/episodes/Episode.java
+++ src/main/java/com/spotify/objects/episodes/Episode.java
@@ -1,10 +1,77 @@
 package com.spotify.objects.episodes;
 
+import com.spotify.objects.SpotifyField;
 import com.spotify.objects.SpotifyObject;
+import com.spotify.objects.SpotifyOptional;
+import com.spotify.objects.chapters.ChapterResumePoint;
+import com.spotify.objects.shows.Show;
+import com.spotify.objects.wrappers.SpotifyImage;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
 public class Episode implements SpotifyObject {
 
-    public Episode() {
-    }
+
+    @SpotifyField("audio_preview_url")
+    private String audioPreviewUrl;
+
+    @SpotifyField
+    private String description;
+
+    @SpotifyField("html_description")
+    private String htmlDescription;
+
+    @SpotifyField("duration_ms")
+    private int duration;
+
+    @SpotifyField
+    private boolean explicit;
+
+    @SpotifyField(value = "spotify", path = {"external_urls"})
+    private String externalUrls;
+
+    @SpotifyField
+    private String href;
+
+    @SpotifyField
+    private String id;
+
+    @SpotifyField
+    private SpotifyImage[] images;
+
+    @SpotifyField("is_externally_hosted")
+    private boolean isExternallyHosted;
+
+    @SpotifyField("is_playable")
+    private boolean isPlayable;
+
+    @Deprecated
+    @SpotifyOptional
+    @SpotifyField
+    private String[] languages;
+
+    @SpotifyField
+    private String name;
+
+    @SpotifyField("release_date")
+    private String releaseDate;
+
+    @SpotifyField("resume_point")
+    private ChapterResumePoint resumePoint;
+
+    @SpotifyField
+    private String type;
+
+    @SpotifyField
+    private String uri;
+
+    @SpotifyOptional
+    @SpotifyField(value = "reason", path = {"restrictions"})
+    private String restrictions;
+
+    @SpotifyField
+    private Show show;
 
 }
