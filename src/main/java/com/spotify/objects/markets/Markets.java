a6ab854e7366b7518f050cb3583f66e41fd6289f
Duncan Jones <dj223@outlook.com>
2023-05-21 02:16:49 +0100

    Added docs to markets object

--- src/main/java/com/spotify/objects/markets/Markets.java
+++ src/main/java/com/spotify/objects/markets/Markets.java
@@ -3,13 +3,25 @@ package com.spotify.objects.markets;
 import com.spotify.objects.SpotifyField;
 import com.spotify.objects.SpotifyObject;
 import com.spotify.objects.wrappers.Country;
+import com.spotify.requests.markets.MarketsAvailableGet;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * Represents spotify markets <br>
+ * Serialized from <a href="https://developer.spotify.com/documentation/web-api/reference/get-available-markets">MarketsAvailableGet</a>
+ *
+ * @see MarketsAvailableGet
+ * @see Country
+ * @see SpotifyObject
+ */
 @Setter
 @Getter
 public class Markets implements SpotifyObject {
 
+    /**
+     * An array of {@link Country} markets available
+     */
     @SpotifyField
     private Country[] markets;
 
