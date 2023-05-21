9239ff9e14134729071149665d18b5a52323327b
Duncan Jones <dj223@outlook.com>
2023-05-21 02:14:21 +0100

    Added docs to genre object

--- src/main/java/com/spotify/requests/genres/GenreAvailableGet.java
+++ src/main/java/com/spotify/requests/genres/GenreAvailableGet.java
@@ -2,7 +2,7 @@ package com.spotify.requests.genres;
 
 import com.spotify.SpotifyResponse;
 import com.spotify.objects.SpotifySerialize;
-import com.spotify.objects.genres.Genre;
+import com.spotify.objects.genres.GenreSeeds;
 import com.spotify.requests.AbstractRequest;
 import com.spotify.requests.SpotifyRequest;
 import lombok.Setter;
@@ -10,9 +10,9 @@ import lombok.Setter;
 /**
  * Retrieve a list of available genres seed parameter values for recommendations.
  * <a href="https://developer.spotify.com/documentation/web-api/reference/get-recommendation-genres">Spotify Docs</a>
- * <p>Serializes into {@link Genre} array</p>
+ * <p>Serializes into {@link GenreSeeds} array</p>
  *
- * @see Genre
+ * @see GenreSeeds
  * @see AbstractRequest
  * @see SpotifyRequest
  * @see SpotifySerialize
@@ -20,7 +20,7 @@ import lombok.Setter;
  */
 @Setter
 @SpotifyRequest("recommendations/available-genre-seeds")
-@SpotifySerialize(Genre.class)
+@SpotifySerialize(GenreSeeds.class)
 public class GenreAvailableGet extends AbstractRequest {
 
 }
--- src/test/java/com/spotify/requests/connection/GenreSeedsTests.java
+++ src/test/java/com/spotify/requests/connection/GenreSeedsTests.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class GenreTests extends AbstractConnectionTest {
+public class GenreSeedsTests extends AbstractConnectionTest {
 
     @Test
     @Tag("NetworkTest")
