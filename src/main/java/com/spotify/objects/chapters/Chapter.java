c4b850a2f5f1ae5839bd2b45960c0b45a5b7f1c6
Duncan Jones <dj223@outlook.com>
2023-05-21 01:57:51 +0100

    Added docs to chapter objects

--- src/main/java/com/spotify/objects/chapters/Chapter.java
+++ src/main/java/com/spotify/objects/chapters/Chapter.java
@@ -3,16 +3,27 @@ package com.spotify.objects.chapters;
 import com.spotify.objects.SpotifyField;
 import com.spotify.objects.SpotifyObject;
 import com.spotify.objects.audiobooks.SimplifiedAudiobook;
+import com.spotify.requests.chapters.ChapterGet;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * Represents a spotify chapter<br>
+ * Serialized from <a href="https://developer.spotify.com/documentation/web-api/reference/get-a-chapter">ChapterGet</a>
+ *
+ * @see ChapterGet
+ * @see SimplifiedChapter
+ * @see SimplifiedAudiobook
+ * @see SpotifyObject
+ */
 @Getter
 @Setter
 public class Chapter extends SimplifiedChapter implements SpotifyObject {
 
 
-    //isplayable checl
-
+    /**
+     * The audiobook for which the chapter belongs.
+     */
     @SpotifyField
     private SimplifiedAudiobook audiobook;
 
