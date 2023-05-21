def5d998b1c1a03e715bfafd8108ae5ad2af5794
Duncan Jones <dj223@outlook.com>
2023-05-21 02:11:41 +0100

    Added docs to episode objects

--- src/main/java/com/spotify/objects/episodes/SimplifiedEpisode.java
+++ src/main/java/com/spotify/objects/episodes/SimplifiedEpisode.java
@@ -2,68 +2,31 @@ package com.spotify.objects.episodes;
 
 import com.spotify.objects.SpotifyField;
 import com.spotify.objects.SpotifyObject;
-import com.spotify.objects.SpotifyOptional;
-import com.spotify.objects.chapters.ChapterResumePoint;
-import com.spotify.objects.wrappers.SpotifyImage;
+import com.spotify.objects.shows.Show;
+import com.spotify.objects.shows.ShowEpisodesSearch;
+import com.spotify.objects.wrappers.AbstractSpotifyCompilation;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * Represents a spotify SimplifiedEpisodeObject
+ *
+ * @see Episode
+ * @see ShowEpisodesSearch
+ * @see Show
+ * @see SpotifyObject
+ * @see AbstractSpotifyCompilation
+ */
 @Getter
 @Setter
-public class SimplifiedEpisode implements SpotifyObject {
+public class SimplifiedEpisode extends AbstractSpotifyCompilation implements SpotifyObject {
 
-    @SpotifyField("audio_preview_url")
-    private String audioPreviewUrl;
 
-    @SpotifyField
-    private String description;
-
-    @SpotifyField("html_description")
-    private String htmlDescription;
-
-    @SpotifyField("duration_ms")
-    private int duration;
-
-    @SpotifyField
-    private boolean explicit;
-
-    @SpotifyField(value = "spotify", path = {"external_urls"})
-    private String externalUrls;
-
-    @SpotifyField
-    private String href;
-
-    @SpotifyField
-    private String id;
-
-    @SpotifyField
-    private SpotifyImage[] images;
-
+    /**
+     * True if the episode is hosted outside of Spotify's CDN.
+     */
     @SpotifyField("is_externally_hosted")
     private boolean isExternallyHosted;
 
-    @SpotifyField("is_playable")
-    private boolean isPlayable;
 
-    @SpotifyField
-    private String[] languages;
-
-    @SpotifyField
-    private String name;
-
-    @SpotifyField("release_date")
-    private String releaseDate;
-
-    @SpotifyField("resume_point")
-    private ChapterResumePoint resumePoint;
-
-    @SpotifyField
-    private String type;
-
-    @SpotifyField
-    private String uri;
-
-    @SpotifyOptional
-    @SpotifyField(value = "reason", path = {"restrictions"})
-    private String restrictions;
 }
