7fd1270545e4c896516542843eb0818e28a395ce
Duncan Jones <dj223@outlook.com>
2023-06-05 21:48:06 +0100

    Added docs to me/audiobooks

--- src/main/java/com/spotify/requests/me/audiobooks/MyAudiobooksGet.java
+++ src/main/java/com/spotify/requests/me/audiobooks/MyAudiobooksGet.java
@@ -1,19 +1,37 @@
 package com.spotify.requests.me.audiobooks;
 
+import com.spotify.SpotifyResponse;
+import com.spotify.objects.SpotifySerialize;
 import com.spotify.requests.SpotifyRequest;
 import com.spotify.requests.SpotifyRequestField;
 import com.spotify.requests.SpotifyRequestVariant;
 import com.spotify.requests.util.Scope;
 import lombok.Setter;
 
+/**
+ * Get a list of the audiobooks saved in the current Spotify user's 'Your Music' library.
+ * <a href="https://developer.spotify.com/documentation/web-api/reference/get-users-saved-audiobooks">Spotify Docs</a>
+ * <p>Serializes into {@link ?}</p>
+ *
+ * @see ?
+ * @see SpotifyRequestVariant
+ * @see SpotifyRequest
+ * @see SpotifySerialize
+ * @see SpotifyResponse
+ */
 @Setter
 @SpotifyRequest(value = "me/audiobooks", authorizations = Scope.USER_LIBRARY_READ)
 public class MyAudiobooksGet implements SpotifyRequestVariant {
 
-
+    /**
+     * The maximum number of items to return. Default: 20. Minimum: 1. Maximum: 50.
+     */
     @SpotifyRequestField
     private int limit;
 
+    /**
+     * The index of the first item to return. Default: 0 (the first item). Use with limit to get the next set of items.
+     */
     @SpotifyRequestField
     private int offset;
 
--- src/main/java/com/spotify/requests/me/audiobooks/MyAudiobooksSavedGet.java
+++ src/main/java/com/spotify/requests/me/audiobooks/MyAudiobooksSavedGet.java
@@ -1,19 +1,37 @@
 package com.spotify.requests.me.audiobooks;
 
+import com.spotify.SpotifyResponse;
+import com.spotify.objects.SpotifySerialize;
 import com.spotify.requests.SpotifyRequest;
 import com.spotify.requests.SpotifyRequestField;
 import com.spotify.requests.SpotifyRequestVariant;
 import com.spotify.requests.util.Scope;
 
 
+/**
+ * Check if one or more audiobooks are already saved in the current Spotify user's library.
+ * <a href="https://developer.spotify.com/documentation/web-api/reference/check-users-saved-audiobooks">Spotify Docs</a>
+ *
+ * @see SpotifyRequestVariant
+ * @see SpotifyRequest
+ * @see SpotifySerialize
+ * @see SpotifyResponse
+ */
 @SpotifyRequest(value = "me/audiobooks/contains", authorizations = Scope.USER_LIBRARY_READ)
 public class MyAudiobooksSavedGet implements SpotifyRequestVariant {
 
-
+    /**
+     * A list of the Spotify IDs
+     */
     @SpotifyRequestField
     private final String[] ids;
 
 
+    /**
+     * Initializes the {@link MyAudiobooksSavedGet} request
+     *
+     * @param ids The Spotify IDS of the audiobooks.
+     */
     public MyAudiobooksSavedGet(String... ids) {
         this.ids = ids;
     }
